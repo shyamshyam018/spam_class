d941c811b216c6f62b9eb904aa0d6fbe2cbe4e51
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-21 20:53:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ def main():
 
     # Process user input and make prediction
     if st.button("Predict"):
+        user_input = user_input.reshape(-1, 1)
         prediction = model.predict([user_input])
+        
         st.write("Prediction:", prediction)
 
 if __name__ == '__main__':
