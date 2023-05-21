07305a1e57bfc54f14e281d9d697e72b87c12ed8
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-21 21:35:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,21 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 # Loading the model
 model = joblib.load('email_class.sav')
 
+
+# Load the dataset from the CSV file
+df = pd.read_csv('emails.csv')
+
+# Extract the email texts and labels from the dataset
+emails = df['Email'].tolist()
+labels = df['Label'].tolist()
+
+# Create and fit the TF-IDF vectorizer
+vectorizer = TfidfVectorizer()
+vectorizer.fit(emails)
+
+# Save the vectorizer
+joblib.dump(vectorizer, 'tfidf_vectorizer.pkl')
+
 # Loading the vectorizer
 vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
