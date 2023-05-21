043a9b63b8ae47d71e8575b0fe70be942569ef6d
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-21 21:09:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,49 @@
 import streamlit as st
-import joblib
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 
-# Load the saved model
-model = joblib.load('email_class.sav')
+# Load the entire dataset from the CSV file
+data = pd.read_csv('dataset.csv')
 
-# Initialize vectorizer and tfidf transformer
-vectorizer = CountVectorizer()
-tfidf_transformer = TfidfTransformer()
+# Filter the dataset for the three classes
+fraud_data = data[data['Label'] == 'FRAUD'][:1000]
+normal_data = data[data['Label'] == 'NORMAL'][:1000]
+spam_data = data[data['Label'] == 'SPAM'][:1000]
+
+# Concatenate the data for the three classes
+train_data = pd.concat([fraud_data, normal_data, spam_data])
+
+# Split the dataset into training and testing sets
+train_emails = train_data['Email']
+train_labels = train_data['Label']
+
+# Create a TF-IDF vectorizer
+vectorizer = TfidfVectorizer()
+
+# Vectorize the training data
+train_vectors = vectorizer.fit_transform(train_emails)
+
+# Train the model
+model = LogisticRegression()
+model.fit(train_vectors, train_labels)
 
 def main():
     # Streamlit app code goes here
-    st.title("Custom ML Model Deployment")
+    st.title("Custom Text Classification")
 
     # Input for user
     user_input = st.text_input("Enter your input:")
 
     # Process user input and make prediction
     if st.button("Predict"):
-        # Preprocess the input data
+        # Vectorize the user input
         input_vector = vectorizer.transform([user_input])
-        input_tfidf = tfidf_transformer.transform(input_vector)
-
-        # Reshape the input to a 2D array
-        input_reshaped = input_tfidf.reshape(1, -1)
 
         # Make prediction
-        prediction = model.predict(input_reshaped)
+        prediction = model.predict(input_vector)
 
         st.write("Prediction:", prediction)
 
 if __name__ == '__main__':
-    # Fit the vectorizer with training data
-    training_data = ['final_dataset.csv']  # Add your training data here
-    vectorizer.fit(training_data)
-
     main()
