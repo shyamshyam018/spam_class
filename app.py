d16725a86bc948aeb4b2f14045d1255661a67904
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-21 21:31:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,89 +1,57 @@
-import pickle
 import streamlit as st
-from streamlit_option_menu import option_menu
 import joblib
-
-# Training and saving the model
-model = ...  # Your trained model
-joblib.dump(model, 'email_class.joblib')
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Loading the model
-model = joblib.load('email_class.joblib')
+model = joblib.load('spam_detection_model.pkl')
 
+# Loading the vectorizer
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
-
-
-# sidebar for navigation
+# Sidebar for navigation
 with st.sidebar:
-    selected_category = option_menu('Email Classification System',
-                                    ['Spam', 'Fraud', 'Normal'],
-                                    icons=['exclamation', 'ban', 'check'],
-                                    default_index=0)
+    selected = st.selectbox(
+        'Select an option',
+        ('Email Classification', 'Spam Detection')
+    )
 
-# Category Selection Page
-if selected_category == 'Spam':
-    # page title
-    st.title('Spam Classification using ML')
+# Email Classification Page
+if selected == 'Email Classification':
+    # Page title
+    st.title('Email Classification')
 
-    # getting the input data from the user
-    user_input = st.text_input('Enter an email')
+    # Input text box
+    user_input = st.text_area('Enter the email text', height=200)
 
-    # code for prediction
-    spam_diagnosis = ''
+    # Classify button
+    if st.button('Classify'):
+        # Transform user input using the vectorizer
+        input_vector = vectorizer.transform([user_input])
 
-    # creating a button for prediction
-    if st.button('Classify Email'):
-        spam_prediction = model.predict([user_input])
+        # Make predictions
+        email_class = model.predict(input_vector)[0]
 
-        if spam_prediction[0] == 'SPAM':
-            spam_diagnosis = 'The email is classified as SPAM'
+        # Display the predicted class
+        st.success(f'The email is classified as: {email_class}')
+
+# Spam Detection Page
+elif selected == 'Spam Detection':
+    # Page title
+    st.title('Spam Detection')
+
+    # Input text box
+    message = st.text_area('Enter the message')
+
+    # Predict button
+    if st.button('Predict'):
+        # Transform user input using the vectorizer
+        input_vector = vectorizer.transform([message])
+
+        # Make predictions
+        spam_prediction = model.predict(input_vector)[0]
+
+        # Display the prediction result
+        if spam_prediction == 1:
+            st.warning('This is a spam message.')
         else:
-            spam_diagnosis = 'The email is not classified as SPAM'
-
-    st.success(spam_diagnosis)
-
-
-# Category Selection Page
-if selected_category == 'Fraud':
-    # page title
-    st.title('Fraud Classification using ML')
-
-    # getting the input data from the user
-    user_input = st.text_input('Enter an email')
-
-    # code for prediction
-    fraud_diagnosis = ''
-
-    # creating a button for prediction
-    if st.button('Classify Email'):
-        fraud_prediction = model.predict([user_input])
-
-        if fraud_prediction[0] == 'FRAUD':
-            fraud_diagnosis = 'The email is classified as FRAUD'
-        else:
-            fraud_diagnosis = 'The email is not classified as FRAUD'
-
-    st.success(fraud_diagnosis)
-
-
-# Category Selection Page
-if selected_category == 'Normal':
-    # page title
-    st.title('Normal Classification using ML')
-
-    # getting the input data from the user
-    user_input = st.text_input('Enter an email')
-
-    # code for prediction
-    normal_diagnosis = ''
-
-    # creating a button for prediction
-    if st.button('Classify Email'):
-        normal_prediction = model.predict([user_input])
-
-        if normal_prediction[0] == 'NORMAL':
-            normal_diagnosis = 'The email is classified as NORMAL'
-        else:
-            normal_diagnosis = 'The email is not classified as NORMAL'
-
-    st.success(normal_diagnosis)
+            st.success('This is not a spam message.')
