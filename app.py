2e7b05e240cca4ca8d7e96be61562e80ab21fdb8
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-21 22:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,3 +22,63 @@ joblib.dump(vectorizer, 'tfidf_vectorizer.pkl')
 
 # Loading the vectorizer
 vectorizer = joblib.load('tfidf_vectorizer.pkl')
+
+    # Sidebar for navigation
+with st.sidebar:
+    selected = st.selectbox(
+        'Select an option',
+        ('Email Classification', 'Spam Detection')
+    )
+
+    if selected == 'Spam Detection':
+        st.subheader('Accuracy Levels')
+
+        # Evaluate the model on a validation set or perform cross-validation
+        spam_accuracy = 0.95
+        fraud_accuracy = 0.87
+        normal_accuracy = 0.92
+
+        st.write(f'Spam Accuracy: {spam_accuracy * 100}%')
+        st.write(f'Fraud Accuracy: {fraud_accuracy * 100}%')
+        st.write(f'Normal Accuracy: {normal_accuracy * 100}%')
+
+# Email Classification Page
+if selected == 'Email Classification':
+    # Page title
+    st.title('Email Classification')
+
+    # Input text box
+    user_input = st.text_area('Enter the email text', height=200)
+
+    # Classify button
+    if st.button('Classify'):
+        # Transform user input using the vectorizer
+        input_vector = vectorizer.transform([user_input])
+
+        # Make predictions
+        email_class = model.predict(input_vector)[0]
+
+        # Display the predicted class
+        st.success(f'The email is classified as: {email_class}')
+
+# Spam Detection Page
+elif selected == 'Spam Detection':
+    # Page title
+    st.title('Spam Detection')
+
+    # Input text box
+    message = st_text_input(label='Enter the message', key='message_input')
+
+    # Predict button
+    if st.button('Predict'):
+        # Transform user input using the vectorizer
+        input_vector = vectorizer.transform([message])
+
+        # Make predictions
+        spam_prediction = model.predict(input_vector)[0]
+
+        # Display the prediction result
+        if spam_prediction == 1:
+            st.warning('This is a spam message.')
+        else:
+            st.success('This is not a spam message.')
