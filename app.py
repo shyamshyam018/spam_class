79e5375d629c7d039487f5f4af93733bba88a247
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-21 22:01:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
+import streamlit as st
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+
+# Loading the model
+model = joblib.load('email_class.sav')
+
+# Load the dataset from the CSV file
+df = pd.read_csv('final_dataset.csv', encoding='latin-1')
+
+# Extract the email texts and labels from the dataset
+emails = df['Email'].tolist()
+labels = df['Label'].tolist()
+
+# Create and fit the TF-IDF vectorizer
+vectorizer = TfidfVectorizer()
+vectorizer.fit(emails)
+
+# Save the vectorizer
+joblib.dump(vectorizer, 'tfidf_vectorizer.pkl')
+
+# Loading the vectorizer
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
+
 # Landing page
 st.markdown("# Email Classification Web App")
 st.markdown("This web app classifies emails as spam or not spam.")
